4bc201c7b383a3d965f3972db7ed9b39b3bba6bc
duzduran <ahmetduzduran@gmail.com>
2026-04-05 03:36:04 +0300

    first commit

---
+++ src/data/site.ts
@@ -0,0 +1,4 @@
+export const siteTitle = "Kur'an'da Yapısal İnceleme";
+
+export const siteDescription =
+  "Kur'an metnindeki yapısal, sayısal ve biçimsel düzenleri sade bir dille açıklayan konu merkezli bir inceleme sitesi.";
